5e13c8c75f8d5d3c4869ad701a249fc767516957
Dmytro Koval <d.koval@medical-tribune.pl>
2019-02-12 17:12:24 +0100

    Move code from docaccess

--- lib/casino/activerecord_authenticator.rb
+++ lib/casino/activerecord_authenticator.rb
@@ -4,7 +4,6 @@ require 'bcrypt'
 require 'phpass'
 
 class CASino::ActiveRecordAuthenticator
-
   class AuthDatabase < ::ActiveRecord::Base
     self.abstract_class = true
   end
@@ -51,6 +50,57 @@ class CASino::ActiveRecordAuthenticator
     false
   end
 
+  def validate(username, password, site, user_agent)
+    #without cutting out only the final class name the object has got no method defined by us in model.
+    @model = "#{@options[:table].classify}".constantize
+    #search users by their login or email + include site_name (exclude blocked accounts at first)
+    user = @model.send("find_by_#{@options[:username_column]}_or_email!", username, site, true)
+    user = @model.send("find_by_#{@options[:username_column]}_or_email!", username, site) if user.nil?
+    user.service = site
+    password_from_database = user.send(@options[:password_column])
+    raise ActiveRecord::RecordNotFound if password.blank?
+    if password_from_database.blank? && !user.old_passwords.unused.empty?
+      valid_pass = nil
+      user.old_passwords.unused.each do |pass|
+        password_from_database = pass.password_hash
+        if valid_old_password?(password, password_from_database, pass.password_type, pass.salt)
+          valid_pass = pass
+          break
+        end
+      end
+      if !valid_pass.nil?
+        valid_pass.overwrite_password! password
+        user.renew_api_token
+        user.create_magwet_accesses
+        dev = user.add_device(user_agent)
+        { username: user.send(@options[:username_column])||user.email, extra_attributes: extra_attributes(user), user: user, un: username, site: site, old_pw: true }
+      else
+        Rails.logger.warn "1"
+        false
+      end
+    else
+      if valid_password?(password, password_from_database)
+        user.renew_api_token
+        user.create_magwet_accesses
+        dev = user.add_device(user_agent)
+        { username: user.send(@options[:username_column])||user.email, extra_attributes: extra_attributes(user), user: user, un: username, site: site }
+      else
+        Rails.logger.warn "inside failed"
+        dev = user.add_device(user_agent)
+        user.log_login_failure(site, dev)
+        user.is_minimal_user? ? {user: user, un: username, site: site } : false
+      end
+    end
+
+  rescue ActiveRecord::RecordNotFound
+    Rails.logger.warn "outside failed #{user_agent}"
+    dev = UserDevice.add(user_agent)
+    UserActivityLog.create(log_event: LogEvent.where(name:'login_failed').first,
+      user_device: dev, value: I18n.t( "users.activity_logs.unknown_login_attempt", {username: username, site: site}),
+      created_by_id: User.parent_user.id)
+    false
+  end
+
   def load_user_data(username)
     user = @model.send("find_by_#{@options[:username_column]}!", username)
     user_data(user)
@@ -64,6 +114,7 @@ class CASino::ActiveRecordAuthenticator
   end
 
   def valid_password?(password, password_from_database)
+    Rails.logger.warn "checking password in overwritten Class"
     return false if password_from_database.blank?
     magic = password_from_database.split('$')[1]
     case magic
@@ -74,6 +125,25 @@ class CASino::ActiveRecordAuthenticator
     else
       valid_password_with_unix_crypt?(password, password_from_database)
     end
+  end
+
+  def valid_old_password?(password, password_from_database, password_format, password_salt)
+    case password_format.to_i
+    when OldPassword::Type['md5']
+      valid_password_with_md5?(password, password_from_database)
+    when OldPassword::Type['new_md5']
+      valid_password_with_new_md5_alg?(password_salt+Digest::MD5.hexdigest(password), password_from_database)
+    end
+  end
+
+  def valid_password_with_md5?(password, password_from_database)
+    Digest::MD5.hexdigest(password) == password_from_database
+  end
+
+  def valid_password_with_new_md5_alg?(password, password_from_database)
+    #not sure why but this salt needs to be here (not passed as a variable)
+    #- otherwise the '#' is parsed to some different form and password doesnt match
+    Digest::MD5.hexdigest(password+'++@#$%@***static part of salt***234@#$&$') == password_from_database
   end
 
   def valid_password_with_bcrypt?(password, password_from_database)
